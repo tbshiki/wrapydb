4df23f1c49a4c88d5b187df5594e3b6eb665c897
つぶ <info@tbshiki.com>
2024-04-15 17:59:27 +0900

    Add .code-workspace to .gitignore, update README.md, and add setup.py and wrapydb module

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+NAME = "Wrapydb"
+VERSION = "0.1"
+PYTHON_REQUIRES = ">=3.7"
+INSTALL_REQUIRES = ["pymysql>=1.0.2", "sqlalchemy>=1.4.22", "sshtunnel>=0.4.0", "pandas>=1.3.0"]
+
+AUTHOR = "tbshiki"
+AUTHOR_EMAIL = "info@tbshiki.com"
+URL = "https://github.com/tbshiki/" + NAME
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name=NAME,
+    version=VERSION,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    url=URL,
+    description="A utility for simplified, secure database connections through SSH tunnels using pymysql, sshtunnel, and SQLAlchemy",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=PYTHON_REQUIRES,
+    install_requires=INSTALL_REQUIRES,
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords="Wrapydb, sql, db, ssh",
+)
---
+++ wrapydb/connector .py
@@ -0,0 +1,75 @@
+import pymysql
+from sqlalchemy import create_engine
+
+from sshtunnel import SSHTunnelForwarder
+
+import pandas as pd
+from contextlib import contextmanager
+
+
+class WrapydbConnector:
+    def __init__(self, dct_data):
+        self.dct_data = dct_data
+
+    @contextmanager
+    def _tunnel_context(self):
+        tunnel = SSHTunnelForwarder(
+            (self.dct_data["ssh_host"], int(self.dct_data["ssh_port"])),
+            ssh_username=self.dct_data["ssh_username"],
+            ssh_private_key=self.dct_data["ssh_private_key"],
+            ssh_private_key_password=self.dct_data["ssh_private_key_password"],
+            remote_bind_address=(
+                self.dct_data["db_host"],
+                int(self.dct_data["db_port"]),
+            ),
+        )
+        tunnel.start()
+        try:
+            yield tunnel
+        finally:
+            tunnel.close()
+
+    @contextmanager
+    def _db_connection(self, cursorclass=pymysql.cursors.Cursor):
+        with self._tunnel_context() as tunnel:
+            connection = pymysql.connect(
+                host="127.0.0.1",
+                user=self.dct_data["db_user"],
+                passwd=self.dct_data["db_password"],
+                db=self.dct_data["db_name"],
+                port=tunnel.local_bind_port,
+                cursorclass=cursorclass,
+            )
+            try:
+                yield connection
+            finally:
+                connection.close()
+
+    def _get_database_url(self, tunnel):
+        return f'mysql+pymysql://{self.dct_data["db_user"]}:{self.dct_data["db_password"]}@127.0.0.1:{tunnel.local_bind_port}/{self.dct_data["db_name"]}'
+
+    def execute_query(self, query, return_dict=False):
+        cursorclass = pymysql.cursors.DictCursor if return_dict else pymysql.cursors.Cursor
+        with self._db_connection(cursorclass=cursorclass) as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+                result = cursor.fetchall()
+                return result
+
+    def execute_update(self, query):
+        with self._db_connection() as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+                connection.commit()
+
+    def df_to_sql(self, df, table_name, if_exists="append", index=False):
+        with self._tunnel_context() as tunnel:
+            database_url = self._get_database_url(tunnel)
+            engine = create_engine(database_url)
+            df.to_sql(name=table_name, con=engine, if_exists=if_exists, index=index)
+
+    def query_to_df(self, query):
+        with self._tunnel_context() as tunnel:
+            database_url = self._get_database_url(tunnel)
+            engine = create_engine(database_url)
+            return pd.read_sql_query(query, engine)
